d15d4ceef90b6e75de89c2a91727da9d14779a0e
letsch <eduardoletsch@hotmail.com>
2020-04-19 09:28:37 -0300

    Parsing with eval

--- index.js
+++ index.js
@@ -1,68 +1,50 @@
 const token = (type, value) => ({
-    type: type,
-    value: value,
+  type: type,
+  value: value,
+});
+
+
+const tokenizer = (str) => {
+  tokens = [];
+
+  str.split("").forEach((element) => {
+    if (isNaN(element)) {
+      tokens.push(token("operator", element));
+    } else if (element != " ") {
+      tokens.push(token("integer", element));
+    }
   });
-  
-  const compare = (expectedType, type) => expectedType == type;
-  
-  const tokenizer = (str) => {
-    tokens = [];
-  
-    str.split("").forEach((element) => {
-      if (isNaN(element)) {
-        tokens.push(token(typeof element, element));
-      } else if (element != " ") {
-        tokens.push(token("integer", parseInt(element)));
+
+  return tokens;
+};
+
+const interprete = (tokens) => {
+  let pointer = 0;
+  let expression = [];
+  let acc = "";
+
+  while (pointer < tokens.length) {
+
+    if (tokens[pointer]["type"] != "operator" && pointer != tokens.length - 1) {
+      acc += tokens[pointer]["value"];
+    } else {
+
+      if (acc == "") {
+        expression.push(parseInt(tokens[pointer]["value"]));
+        break;
       }
-    });
-  
-    return tokens;
-  };
-  
-  const interprete = (tokens) => {
-    //console.log(tokens);
-  
-    let pointer = 0;
-    let left = "";
-    let right = "";
-    let token = tokens[pointer];
-    let operator;
-    //console.log(token);
-  
-    while (token["type"] == "integer") {
-      left += token["value"];
-      pointer++;
-      token = tokens[pointer];
+
+      acc = parseInt(acc);
+      expression.push(acc);
+      expression.push(tokens[pointer]["value"]);
+      acc = "";
     }
+    pointer++;
+  }
+
+  return eval(expression.join(''));
+
   
-    left = parseInt(left);
-  
-    operator = token["value"];
-    tokens.splice(0, pointer + 1);
-  
-    //let right = parseInt(tokens.reduce((accum, value) => accum + value)["value"]);
-    //let right = parseInt(tokens.join("")["value"]);
-  
-    tokens.forEach((element) => {
-      right += element["value"];
-    });
-    right = parseInt(right);
-  
-    switch (operator) {
-      case "+":
-        return left + right;
-  
-      case "-":
-        return left - right;
-  
-      case "/":
-        return left / right;
-  
-      case "*":
-        return left * right;
-    }
-  };
-  
-  //console.log(interprete(tokenizer("5 + 5")));
-  console.log(interprete(tokenizer("101 + 155")));
-  
+};
+
+console.log(interprete(tokenizer("3 + 5 * 5")));
